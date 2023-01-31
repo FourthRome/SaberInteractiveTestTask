025e8b1c62b2cf0e51ce3a463e68f4e72359e694
FourthRome <romansdidnotcrucify@gmail.com>
2023-01-31 16:59:14 +0300

    Add (de)serialization (no error handling yet)

--- task_3.cpp
+++ task_3.cpp
@@ -1,10 +1,10 @@
 #include <cstdio>
-#include <fstream>
 #include <iostream>
 #include <map>
 #include <stdexcept>
 #include <string>
 #include <vector>
+
 
 struct ListNode {
     ListNode* prev = nullptr;
@@ -19,12 +19,20 @@ public:
     void Serialize(FILE* file);
     void Deserialize(FILE* file);
     void PushBack(ListNode* node);
-    void Validate();
+    void Print();
+
+    virtual ~List() {
+        delete[] _storage;
+    }
 
 private:
+    void Validate();
+
     ListNode* head = nullptr;
     ListNode* tail = nullptr;
     int count = 0;
+
+    ListNode* _storage = nullptr;
 };
 
 void List::PushBack(ListNode* node) {
@@ -37,40 +45,18 @@ void List::PushBack(ListNode* node) {
         tail = node;
         tail->prev = temp;
     }
+    count += 1;
 }
 
-// void List::Validate() {
+void List::Print() {
+    ListNode* ptr = head;
+    while (ptr) {
+        std::cout << ptr->data << std::endl;
+        ptr = ptr->next;
+    }
+}
 
-
-
-//     std::map<ListNode*, size_t> ids;
-//     ListNode* current_ptr = head;
-//     ListNode* prev_ptr = nullptr;
-//     size_t current_node_idx;
-
-//     // 
-//     while (current_ptr) {
-//         // List integrity checks
-//         if (current_ptr->prev != prev_ptr) {
-//             throw std::runtime_error(
-//                     "Incorrect prev pointer in a node");
-//         }
-//         if (ids.find(current_ptr) != ids.end()) {
-//             throw std::runtime_error(
-//                     "Found a cycle in list's graph");
-//         }
-
-//         ids.emplace(current_ptr, current_node_idx);
-        
-//         // Move on to the next node
-//         prev_ptr = current_ptr;
-//         current_ptr = current_ptr->next;
-//         current_node_idx++;
-//     } while (current_ptr);
-//     if (current_node_idx != count) {
-//         throw std::runtime_error("Number of nodes does not match count");
-//     }
-// }
+void List::Validate() {}
 
 void List::Serialize(FILE* file) {
     if (file == NULL) {
@@ -94,19 +80,22 @@ void List::Serialize(FILE* file) {
         if (node_ptr->rand != nullptr) {
             buffer[node_idx] = node_ids.find(node_ptr->rand)->second;
         }
+        node_ptr = node_ptr->next;
     }
 
     // Serialize all data into file
-    std::fwrite(&count, sizeof(count), 1, file);
-    std::fwrite(buffer.data(), buffer.size(), 1, file);
+    size_t byte_counter = 0;
+    byte_counter += std::fwrite(&count, sizeof(count), 1, file);
+    byte_counter += std::fwrite(buffer.data(), buffer.size() * sizeof(int), 1, file);
     node_ptr = head;
     for (size_t node_idx = 0; node_idx < count; ++node_idx) {
-        auto string_len = node_ptr->data.size() + 1;
-        std::fwrite(&string_len, sizeof(string_len), 1, file);
-        std::fwrite(node_ptr->data.c_str(), string_len, 1, file);
+        size_t string_len = node_ptr->data.size() + 1;
+        byte_counter += std::fwrite(&string_len, sizeof(string_len), 1, file);
+        byte_counter += std::fwrite(node_ptr->data.c_str(), string_len * sizeof(char), 1, file);
         node_ptr = node_ptr->next;
     }
 }
+
 
 void List::Deserialize(FILE* file) {
     if (file == NULL) {
@@ -114,57 +103,58 @@ void List::Deserialize(FILE* file) {
                 "Failed to deserialize List: incorrect FILE* object");
     }
 
+    // TODO: clear the object before deserialization
+
+    size_t fread_counter = 0;
+
+    // Read count
     int count;
-    std::fread(&count, sizeof(count), 1, file);
-    
+    fread_counter += std::fread(&count, sizeof(count), 1, file);
+    this->count = count;
+    if (count <= 0) return;  // TODO: add corrupted file's handling: count < 0?
+
+    // Read links
     int* rand_links = new int[count];
-    std::fread(&rand_links, count * sizeof(*rand_links), 1, file);
+    fread_counter += std::fread(rand_links, count * sizeof(*rand_links), 1, file);
+
+    // Read strings
+    std::vector<std::string> strings;
+    size_t string_buffer_len = 4;
+    char* string_buffer = new char[string_buffer_len];
+    for (size_t node_idx = 0; node_idx < count; ++node_idx) {
+        size_t string_len;
+        fread_counter += std::fread(&string_len, sizeof(string_len), 1, file);
+        if (string_len > string_buffer_len) {
+            delete[] string_buffer;
+            string_buffer_len = 2 * string_len;
+            string_buffer = new char[string_buffer_len];
+        }
+        fread_counter += std::fread(string_buffer, string_len, 1, file);
+        strings.push_back(std::string(string_buffer));
+    }
+
+    // Restore List
+    _storage = new ListNode[count];
+    head = _storage;
+    tail = _storage + count - 1;  // No breaking boundaries here, count > 0
+    for (size_t node_idx = 0; node_idx < count; ++node_idx) {
+        ListNode* ptr = _storage + node_idx;
+        ptr->data = strings[node_idx];
+        if (node_idx > 0) {
+            ptr->prev = ptr - 1;
+            (ptr - 1)->next = ptr;
+        }
+        if (rand_links[node_idx] != -1) {
+            ptr->rand = _storage + rand_links[node_idx];
+        }
+    }
+
+    delete[] rand_links;
+    delete[] string_buffer;
 }
 
+
 int main () {
-    // FILE* output = fopen("./test.bin", "wb");
-    // if (output == NULL) {
-    //     std::cerr << "File not opened" << std::endl;
-    //     return -1;
-    // }
-    // const std::string out = "Hello";
-    // int count = out.size() + 1;
-    // if (std::fwrite(&count, sizeof(count), 1, output) != 1) {
-    //     std::cout << "Failed to write properly" << std::endl;
-    //     return -1;
-    // }
-    // if (std::fwrite(out.c_str(), count, 1, output) != 1) {
-    //     std::cout << "Failed to write properly" << std::endl;
-    //     return -1;
-    // }
-    // // std::fwrite(out.c_str(), out.size(), 1, output);
-    // fclose(output);
-
-
-    // FILE* input = fopen("./test.bin", "rb");
-    // if (input == NULL) {
-    //     std::cerr << "File not opened" << std::endl;
-    //     return -1;
-    // }
-    // int count_new;
-    // if (fread(&count_new, sizeof(count_new), 1, input) != 1) {
-    //     std::cerr << "Failed to read properly" << std::endl;
-    //     return -1;
-    // }
-    // char* string_buffer = new char[count_new];
-    // if (fread(string_buffer, count_new * sizeof(*string_buffer), 1, input) != 1) {
-    //     std::cerr << "Failed to read properly" << std::endl;
-    //     return -1;
-    // }
-    // std::cout << count_new << std::endl;
-
-    // string_buffer[count_new - 1] = '\0';
-    // std::string new_string(string_buffer);
-
-    // std::cout << new_string << std::endl;
-
-    // delete[] string_buffer;
-
     // Create a list    
     List list;
     ListNode* nodes = new ListNode[3];
@@ -176,10 +166,19 @@ int main () {
     list.PushBack(nodes + 1);
     list.PushBack(nodes + 2);
 
+    list.Print();
+
     // Write list contents to file
     FILE* output = fopen("test.bin", "wb");
+    list.Serialize(output);
+    fclose(output);
 
-    //
+    // Read contents back from file
+    FILE* input = fopen("test.bin", "rb");
+    List new_list;
+    new_list.Deserialize(input);
+    new_list.Print();
+    fclose(input);
 
     delete[] nodes;
     return 0;
